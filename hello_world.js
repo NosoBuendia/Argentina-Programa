199790833d95d75e1d724ce740e89c3f910a23b2
Gonzalo <nosobuendia@gmail.com>
2023-05-18 21:11:51 -0300

    tercer y ultimo cambio

--- hello_world.js
+++ hello_world.js
@@ -1,2 +1,8 @@
 let nombre="Juan Carlos"
-console.log("Hello ", nombre, " este es un segundo cambio.")
+console.log("Hello ", nombre, " este es el tercer y ultimo cambio.")
+
+let palabra="capibara "
+for(let cont=0;cont<3;cont++){
+     console.log(palabra)
+    
+}
